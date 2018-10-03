57ef32322a7115c0c9cdd68180a09e0370bb15c5
Regi <regi@Regis-MacBook-Pro.local>
2018-10-03 17:24:45 +0700

    stable

---
+++ vendor/laravolt/indonesia/src/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Laravolt\Indonesia;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind('indonesia', function() {
+            return new Indonesia;
+        });
+
+        $this->commands(\Laravolt\Indonesia\Commands\SeedCommand::class);
+
+    }
+
+    public function boot()
+    {
+        if ($this->isLaravel53AndUp()) {
+            $this->loadMigrationsFrom(__DIR__.'/migrations');
+        } else {
+            $this->publishes([
+                __DIR__ . '/migrations' => $this->app->databasePath() . '/migrations'
+            ], 'migrations');
+        }
+
+        if (class_exists(Application::class)) {
+            $this->publishes(
+                [
+                    __DIR__.'/../config/indonesia.php' => config_path('laravolt/indonesia.php'),
+                ], 'config'
+            );
+        }
+    }
+
+    protected function isLaravel53AndUp()
+    {
+        return version_compare($this->app->version(), '5.3.0', '>=');
+    }
+}
