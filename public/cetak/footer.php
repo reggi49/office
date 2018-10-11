ce2f19f471ab07a361a68677d14307ec79213b58
reggi49 <reggimuhamad@gmail.com>
2018-10-11 11:28:13 +0700

    fixed untracked files

--- public/cetak/footer.php
+++
@@ -1,9 +0,0 @@
-    <!-- Right Panel -->
-
-    <script src="../js/vendor/jquery-2.1.4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
-    <script src="../js/main.js"></script>
-    <script src="../js/plugins.js"></script>
-
-</body>
-</html>
