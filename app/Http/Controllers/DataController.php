7ea780e81471c5a2b405b67af943925962c50832
reggi49 <reggimuhamad@gmail.com>
2018-10-11 11:54:42 +0700

    update selft refresh

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -148,7 +148,7 @@ class DataController extends Controller
         }else{
             $datapro->save();
         }
-        return redirect('data')->with('success', 'Information has been updated');
+        redirect(Request::url());
     }
 
     /**
