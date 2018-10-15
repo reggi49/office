1a03fbd009b24a5366b8064dc4a6e4f98ebd90fe
reggi49 <reggimuhamad@gmail.com>
2018-10-15 10:14:18 +0700

    trigger update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
 
             <div class="col-sm-12">
                 <div class="alert  alert-success alert-dismissible fade show" role="alert">
-                  <span class="badge badge-pill badge-success">Info</span> Selamat datang {{ Auth::user()->name }} di aplikasi mbtech office.
+                  <span class="badge badge-pill badge-success">Info</span> Selamat datang {{ Auth::user()->name }} di aplikasi MBtech Office.
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
