d196b172809449b30158803b3a4b99b8d141ba2a
reggi49 <reggimuhamad@gmail.com>
2020-06-06 09:36:06 +0700

    change destination

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -181,14 +181,14 @@ class DataController extends Controller
         $image = $request->file('gambar1');
         $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
         
-        $destinationPath = public_path().'/images/thumbnail';
+        $destinationPath = $string = rtrim(public_path(),"/").'_html/newoffice/images/thumbnail';
         Image::configure(array('driver' => 'imagick'));
         $img = Image::make($image->getRealPath());
         $img->resize(250, 150, function ($constraint) {
             $constraint->aspectRatio();
         })->save($destinationPath.'/'.$input['imagename']);
    
-        $destinationPath = public_path().'/images';
+        $destinationPath = rtrim(public_path(),"/").'_html/newoffice/images';
         $image->move($destinationPath, $input['imagename']);
    
         // $this->postImage->add($input);
