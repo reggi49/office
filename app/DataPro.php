063a759db4bc2e43bef4802332f1e362411d31a2
reggi49 <reggimuhamad@gmail.com>
2020-07-23 14:58:12 +0700

    rollback

--- app/DataPro.php
+++ app/DataPro.php
@@ -4,13 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use DB;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
 
 class DataPro extends Model
 {
-    use SoftDeletes;
     protected $fillable =['id','region','provinsi','kota','klasifikasi','subklasifikasi','kriteria','toko','alamat','alamat2','phone','faxs','email','hp','contact','b_day','alamat_rumah','religion','celebration','status','gambar','gambar2','id','id_prov','id_kota','keterangan'];
-    protected $dates=['d_day','deleted_at'];
+    protected $dates=['d_day'];
     
 }
