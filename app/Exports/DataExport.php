93d5f1e3885d935f6e296721e215c930eb352601
reggi49 <reggimuhamad@gmail.com>
2023-07-14 15:35:25 +0700

    add heading 2

--- app/Exports/DataExport.php
+++ app/Exports/DataExport.php
@@ -20,7 +20,7 @@ class DataExport implements FromCollection, WithHeadings
     public function headings(): array
     {
         return [
-            'region', 'provinsi', 'kota', 'klasifikasi', 'subklasifikasi', 'kriteria', 'toko', 'alamat', 'alamat_2', 'phone', 'faxs', 'email', 'hp', 'contact', 'b_day', 'alamat_rumah', 'religion', 'celebration', 'status', 'latitude', 'longitude', 'keterangan', 'deleted_at'
+            'Region', 'Provinsi', 'Kota', 'Klasifikasi', 'Subklasifikasi', 'Kriteria', 'Toko', 'Alamat', 'Alamat 2', 'No Telp', 'Faxs', 'Email', 'No Handphone', 'Kontak', 'Tanggal Lahir', 'Alamat Rumah', 'Agama', 'Hari Raya', 'Status', 'Latitude', 'Longitude', 'Keterangan', 'Tangggal Di-delete'
         ];
     }
 
