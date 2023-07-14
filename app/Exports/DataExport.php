a293a7eb8d273671adfed1ec53de09062b4aac7f
reggi49 <reggimuhamad@gmail.com>
2023-07-14 15:32:27 +0700

    add heading

--- app/Exports/DataExport.php
+++ app/Exports/DataExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\DataPro;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class DataExport implements FromCollection
+class DataExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -15,4 +16,12 @@ class DataExport implements FromCollection
         // return DataPro::all();
         return DataPro::select('region', 'provinsi','kota','klasifikasi','subklasifikasi','kriteria','toko','alamat','alamat_2','phone','faxs','email', 'hp','contact','b_day','alamat_rumah','religion','celebration','status','latitude','longitude','keterangan', 'deleted_at')->get();
     }
+
+    public function headings(): array
+    {
+        return [
+            'region', 'provinsi', 'kota', 'klasifikasi', 'subklasifikasi', 'kriteria', 'toko', 'alamat', 'alamat_2', 'phone', 'faxs', 'email', 'hp', 'contact', 'b_day', 'alamat_rumah', 'religion', 'celebration', 'status', 'latitude', 'longitude', 'keterangan', 'deleted_at'
+        ];
+    }
+
 }
